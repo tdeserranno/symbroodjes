6c26862e8cd04fd3499132969350a0be0b6d41ff
Thomas Deserranno <thomas.deserranno@gmail.com>
2014-04-08 13:37:22 +0200

    topping entity added

---
+++ src/Broodjes/BroodjesBundle/Entity/Topping.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Broodjes\BroodjesBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Topping
+ */
+class Topping
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var float
+     */
+    private $price;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Topping
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set price
+     *
+     * @param float $price
+     * @return Topping
+     */
+    public function setPrice($price)
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    /**
+     * Get price
+     *
+     * @return float 
+     */
+    public function getPrice()
+    {
+        return $this->price;
+    }
+}
